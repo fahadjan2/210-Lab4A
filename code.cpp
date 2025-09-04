b35477a61520d3a8fd266245ff46690200fa11dd
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-04 16:23:12 -0700

    committing

--- code.cpp
+++ code.cpp
@@ -2,6 +2,8 @@
 //IDE used: Visual Studio
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -12,6 +14,21 @@ struct Color{
 };
 
 int main(){
+    srand(time(0));
+    int n = rand() % 50 + 25;
+
     vector<Color> colors;
-    
+
+    for (int i = 0; i < n; i++){
+        int randRed = rand() % 255;
+        int randGreen = rand() % 255;
+        int randBlue = rand() % 255;
+        
+        Color tempColors = {randRed, randGreen, randBlue};
+        colors.push_back(tempColors);
+    }
+
+
+    cout << "e" << endl;
+    //cout << colors[1].red << colors[1].green << colors[1].blue << endl;
 }
