e25be9966dd60a585e70896b2ce24ca36ef4b113
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-04 16:30:55 -0700

    final commit

--- code.cpp
+++ code.cpp
@@ -16,9 +16,10 @@ struct Color{
 int main(){
     srand(time(0));
     int n = rand() % 50 + 25;
+    int numberOfValues;
 
     vector<Color> colors;
-
+    
     for (int i = 0; i < n; i++){
         int randRed = rand() % 255;
         int randGreen = rand() % 255;
@@ -26,9 +27,13 @@ int main(){
         
         Color tempColors = {randRed, randGreen, randBlue};
         colors.push_back(tempColors);
+
+        numberOfValues++;
     }
 
-
-    cout << "e" << endl;
-    //cout << colors[1].red << colors[1].green << colors[1].blue << endl;
+    //Printing
+    cout << "Color#\tR Value\tG Value\tB Value\n-------\t-------\t-------\t-------" << endl;
+    for (int i = 0; i < numberOfValues; i++){
+        cout << "  " << i << " \t  " << colors[i].red << "\t  " << colors[i].green << "\t  " << colors[i].blue << endl;
+    }
 }
