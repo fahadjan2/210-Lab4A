eb46b2835a7f0e83f663bb0763fc52c2934ca067
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-04 16:12:31 -0700

    committing

---
+++ code.cpp
@@ -0,0 +1,17 @@
+//COMSC-210 | Lab 4A | Fahad Fawad Ahmad
+//IDE used: Visual Studio
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Color{
+    int red;
+    int green;
+    int blue;
+};
+
+int main(){
+    vector<Color> colors;
+    
+}
